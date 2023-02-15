35647ef675987de1584efcc212b5270bf0ad28a6
Leonardo Telles <leo.c.telles@gmail.com>
2023-02-15 11:09:30 -0300

    first commit

---
+++ src/main/java/com/github/leotelles/ipaddresstracker/IpAddressTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.leotelles.ipaddresstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpAddressTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpAddressTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leotelles/ipaddresstracker/IpAddressTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leotelles.ipaddresstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpAddressTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
